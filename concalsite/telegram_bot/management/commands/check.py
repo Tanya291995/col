ac2e1ad9b6de2a34a646a76bf0343501e15ccda6
tanya95 <tanya14-1995@mail.ru>
2017-02-28 14:03:49 +0300

    Simple user interface

---
+++ concalsite/telegram_bot/management/commands/check.py
@@ -0,0 +1,69 @@
+from telegram_bot import models
+from django.core.management.base import BaseCommand
+import random
+
+class Command(BaseCommand):
+    # python manage.py createtags --help
+    help = 'Check'
+
+    def calculate_confidence(self, count_correct_answer, count_conf_interval, count_questions):
+        sum = count_correct_answer + 0.5 * count_conf_interval
+        confidence = (sum/count_questions) * 100
+        return confidence
+
+    def handle(self, *args, **options):
+    	#count of questions
+        count_questions = 10
+        #numbers of correct answers
+        count_correct_answer = 0
+        #numbers of answers with confidence = 50%
+        count_conf_interval = 0
+        user_confidence = 0
+        #get a list of all State`s id
+        id_list_corr = models.State.objects.values_list('id', flat = True)
+
+        #get random State`s id
+        rand_id_list = random.sample(list(id_list_corr), count_questions)
+
+        for i in range(0, count_questions):
+            right_answer = ""
+            fact = models.State.objects.filter(id = rand_id_list[i])
+            country_name = fact.values_list('name', flat = True)
+            flag = random.choice([0, 1])
+            if (flag == 0):
+                president = fact.values_list('head', flat = True)
+                right_answer = "да"
+            else:
+                rand = random.choice(id_list_corr)
+                other_fact = models.State.objects.filter(id = rand)
+                president = other_fact.values_list('head', flat = True)
+                if(fact.values_list('name', flat = True)[0] == other_fact.values_list('name', flat = True)[0]):
+                    right_answer = "да"
+                else:
+                    right_answer = "нет"
+
+            question = str(i+1) + ") Верно ли, что " + president[0] + " является главой государства " + country_name[0] + "?"
+            print(question)
+            print("Правильный ответ :", right_answer)
+            user_answer = input('Введите ваш ответ:')
+            user_conf = int(input('Введите вашу уверенность:'))
+
+            user_confidence+= user_conf
+            if((user_answer == right_answer) & (user_conf == 50)):
+                count_conf_interval+= 1
+                print(str(count_conf_interval))
+            else:
+                if(user_answer == right_answer):
+                    count_correct_answer+= 1
+                    print(str(count_correct_answer))
+
+            models.QuestionSession.objects.create(id_question = models.State.objects.get(id = rand_id_list[i]),
+            	id_session = 1, user_answer = user_answer, confidence = user_confidence)
+
+
+        total_confidence = user_confidence/10
+        really_confidence = self.calculate_confidence(count_correct_answer, count_conf_interval, count_questions)
+
+        print("Ваша уверенность: " + str(total_confidence) + "%")
+        print("Ваша реальная уверенность: " + str(really_confidence) + "%")
+
